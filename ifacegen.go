d7a4383c58da2a0c68cc1b7d1c6d50bb73944c5d
bin zhang <binz@uber.com>
2019-02-02 00:08:40 -0800

    use types.Qualifier to not print package name

--- ifacegen.go
+++ ifacegen.go
@@ -29,41 +29,71 @@ import (
 	"golang.org/x/tools/imports"
 )
 
-func main() {
-	receiver, mockPkg, output, srcPath, ifaceName := parseFlag()
+// TODO:
+// - handle interface that is in vendor
 
-	parsed := Interface{
-		Pkg:       mockPkg,
+func main() {
+	receiver, output, srcPath, ifaceName, mock, mockInTest := parseFlag()
+
+	iface := Interface{
 		Interface: ifaceName,
 		Receiver:  receiver,
-		Methods:   parseMethods(srcPath, ifaceName),
 	}
 
-	b := genCode(&parsed)
+	pkgQual, pkgName := qualifyPackage(mockInTest)
+	if mock {
+		iface.PkgName = pkgName
+	}
+
+	iface.Methods = parseMethods(pkgQual, srcPath, ifaceName)
+
+	b := genCode(&iface)
 	writeCode(output, b)
 }
 
-func parseFlag() (receiver, mockPkg, output, srcPath, iface string) {
-	flag.StringVar(&receiver, "r", "", "name of receiver, default to *{Interface}{Gen|Mock}")
-	flag.StringVar(&mockPkg, "p", "", "package name of the mock struct")
-	flag.StringVar(&output, "o", "", "name of output file, default to os.Stdout")
-	var ifaceName string
-	flag.StringVar(&ifaceName, "i", "", "interface name, {import path}.{interface}, e.g. net/http.Handler; required")
-	flag.Parse()
+func qualifyPackage(mockInTest bool) (types.Qualifier, string) {
+	pkg := importPackage("")
+	log.Printf("this pkg: name:%s,dir:%s,path:%s", pkg.Name, pkg.Dir, pkg.ImportPath)
+	name := pkg.Name
+	if mockInTest {
+		name += "_test"
+	}
 
-	i := strings.LastIndex(ifaceName, ".")
-	if i <= 0 || i+1 == len(ifaceName) {
-		fmt.Fprintln(os.Stderr, "format of interface name is {import path}.{interface}, e.g. `net/http.Handler`")
+	tpkg := types.NewPackage(pkg.ImportPath, name)
+	q := func(pkg *types.Package) string {
+		log.Printf("thispkg.path:%s pkg.path:%s", tpkg.Path(), pkg.Path())
+		if pkg.Path() == tpkg.Path() || pkg.Name() == name {
+			return ""
+		}
+		return pkg.Name()
+	}
+	return q, name
+}
+
+func parseFlag() (receiver, output, srcPath, ifaceName string, mock, mockInTest bool) {
+	flag.StringVar(&receiver, "r", "", "name of receiver, default to *{Interface}{Gen|Mock}")
+	flag.StringVar(&output, "o", "", "name of output file, default to os.Stdout")
+	flag.StringVar(&ifaceName, "i", "", "interface name, [{import path}.]{interface}, e.g. net/http.Handler, Foo; required")
+	flag.BoolVar(&mock, "m", false, "true to generate mock struct")
+	flag.BoolVar(&mockInTest, "t", false, "true to put the mock struct in test package")
+	flag.Parse()
+	if ifaceName == "" {
+		fmt.Fprintln(os.Stderr, "interface name is required")
 		os.Exit(1)
 	}
 
-	return receiver, mockPkg, output, ifaceName[:i], ifaceName[i+1:]
+	i := strings.LastIndex(ifaceName, ".")
+	if i >= 0 {
+		srcPath = ifaceName[:i]
+		ifaceName = ifaceName[i+1:]
+	}
+	return receiver, output, srcPath, ifaceName, mock, mockInTest
 }
 
-func genCode(parsed *Interface) []byte {
+func genCode(iface *Interface) []byte {
 	var tpl *template.Template
 	var sfx string
-	if parsed.Pkg != "" {
+	if iface.PkgName != "" {
 		tpl = mockTpl
 		sfx = "Mock"
 	} else {
@@ -71,19 +101,19 @@ func genCode(parsed *Interface) []byte {
 		sfx = "Gen"
 	}
 
-	if parsed.Receiver == "" {
-		parsed.Receiver = "*" + parsed.Interface + sfx
+	if iface.Receiver == "" {
+		iface.Receiver = "*" + iface.Interface + sfx
 	}
-	parsed.Struct = strings.TrimPrefix(parsed.Receiver, "*")
+	iface.Struct = strings.TrimPrefix(iface.Receiver, "*")
 
 	var buf bytes.Buffer
-	err := tpl.Execute(&buf, parsed)
+	err := tpl.Execute(&buf, iface)
 	if err != nil {
 		log.Fatalf("execute template:%s err:%v", tpl.Name(), err)
 	}
 	gen := buf.Bytes()
 
-	if parsed.Pkg == "" {
+	if iface.PkgName == "" {
 		gen, err = format.Source(gen)
 	} else {
 		gen, err = imports.Process("", gen, nil)
@@ -116,18 +146,32 @@ type Method struct {
 }
 
 type Interface struct {
-	Pkg       string
+	PkgName   string
 	Interface string
 	Struct    string
 	Receiver  string
 	Methods   []*Method
 }
 
-func parseMethods(srcPath, ifaceName string) []*Method {
-	pkg, err := build.Import(srcPath, "", 0)
-	if err != nil {
-		log.Fatalf("import pkg:%q err:%v", srcPath, err)
+func importPackage(srcPath string) *build.Package {
+	// package is e.g. "net/http" if it is in GOROOT or GOPATH,
+	// package is e.g. "github.com/foo/bar/vendor/golang.org/x/tools/imports" if it is a vendor package.
+	var pkg *build.Package
+	var err error
+	if srcPath != "" {
+		pkg, err = build.Import(srcPath, "", 0)
+		fatalOnErr(err, "import pkg:%s", srcPath)
+	} else {
+		wd, ee := os.Getwd()
+		fatalOnErr(ee, "getwd")
+		pkg, err = build.ImportDir(wd, 0)
+		fatalOnErr(err, "importdir wd:%q", wd)
 	}
+	return pkg
+}
+
+func parseMethods(pkgQual types.Qualifier, srcPath, ifaceName string) []*Method {
+	pkg := importPackage(srcPath)
 
 	var srcFiles []string
 	for _, fn := range pkg.GoFiles {
@@ -138,7 +182,7 @@ func parseMethods(srcPath, ifaceName string) []*Method {
 
 	var methods []*Method
 	for i := 0; i < iface.NumMethods(); i++ {
-		methods = append(methods, parseMethod(iface.Method(i)))
+		methods = append(methods, parseMethod(pkgQual, iface.Method(i)))
 	}
 	return methods
 }
@@ -171,12 +215,12 @@ func findInterface(info *types.Info, name string) *types.Interface {
 	return nil
 }
 
-func parseTuple(tuple *types.Tuple, namePrefix string) (params, args string) {
+func parseTuple(pkgQual types.Qualifier, tuple *types.Tuple, namePrefix string) (params, args string) {
 	var errNameUsed bool
 	for i := 0; i < tuple.Len(); i++ {
 		v := tuple.At(i)
 		name := v.Name()
-		ty := v.Type().String()
+		ty := types.TypeString(v.Type(), pkgQual)
 
 		if name == "" && namePrefix != "" {
 			if ty == "error" && !errNameUsed {
@@ -190,25 +234,31 @@ func parseTuple(tuple *types.Tuple, namePrefix string) (params, args string) {
 			params += ", "
 			args += ", "
 		}
-		params += name + " " + v.Type().String()
+		params += name + " " + ty
 		args += name
 	}
 	return params, args
 }
 
-func parseMethod(fn *types.Func) *Method {
+func parseMethod(pkgQual types.Qualifier, fn *types.Func) *Method {
 	sig := fn.Type().(*types.Signature)
 
-	params, args := parseTuple(sig.Params(), "a")
-	results, resvar := parseTuple(sig.Results(), "r")
+	params, args := parseTuple(pkgQual, sig.Params(), "a")
+	results, resvar := parseTuple(pkgQual, sig.Results(), "r")
 
 	return &Method{
 		Method:     fn.Name(),
-		Sig:        sig.String(),
+		Sig:        types.TypeString(sig, pkgQual),
 		Params:     params,
 		Args:       args,
 		Results:    results,
 		ResultVars: resvar,
+	}
+}
+
+func fatalOnErr(err error, format string, args ...interface{}) {
+	if err != nil {
+		log.Fatalf(format+" err:"+err.Error(), args...)
 	}
 }
 
@@ -221,7 +271,7 @@ func (m {{$x.Receiver}}) {{.Method}}({{.Params}}) ({{.Results}}) {
 
 var mockTpl = template.Must(template.New("mock").Parse(`// @generated by ifacegen
 {{with $x := .}}
-package {{$x.Pkg}}
+package {{$x.PkgName}}
 
 const (
   {{range $i, $m := $x.Methods}}call{{$m.Method}} = {{$i}}
